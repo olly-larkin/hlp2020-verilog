6191cd678bd2bdfbceb766ca2a59384ba0c8076e
NGovani <naim@govani.org>
2020-02-23 20:36:26 +0000

    Added initial waveform module

---
+++ ng2517/main.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+module Verishot.Main
+
+
+
+open System
+open Verishot.Waveform
+open Verishot.CoreTypes
+
+
+let testInput = [
+
+]
+
+let state:WaveformState =
+    {
+        prevVal=0;
+        coords=(10,01);
+        svgVals = []
+    }
+
+[<EntryPoint>]
+let main argv =    
+    List.map Waveform.ModuleWaveform testInput
+    0 // return an integer exit code
