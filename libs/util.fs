9e7467ec34068aff5803013309a5870ddcdd2aeb
Lin Hao Lee <lhlee888@hotmail.com>
2020-02-23 18:07:02 +0000

    Update util.fs

--- libs/util.fs
+++ libs/util.fs
@@ -1,8 +1,20 @@
 module Verishot.Util
 
+open System.IO
+
+let print x = printfn "%A" x
+
 module List =
     let catOptions (xs: 'a option list): 'a list =
         xs
         |> List.collect (function
             | Some item -> [ item ]
             | None -> [])
+
+let readFileToString (filename: string): string =
+    File.ReadAllLines(filename)
+    |> String.concat "\n"
+
+let writeStringToFile (filename: string) (content: string) =
+    File.WriteAllText(filename, content)
+    
