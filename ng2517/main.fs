f767f9396dc5de64518124a6db161f6948f20f15
NGovani <naim@govani.org>
2020-02-24 14:24:21 +0000

    added functions to iterate over sim input

--- ng2517/main.fs
+++ ng2517/main.fs
@@ -16,20 +16,21 @@ let testInput = [
     0;1;0;1;1;1;0;0;1
 ]
 
-let style = "fill:none;stroke:black;stroke-width:1"
+
+let styleprops = [("fill", "none");("stroke","black");("stroke-width","1")]
 
 let state:WaveformState =
     {
         prevVal=0;
-        svgVals = Polyline([0.0, 3.0],[("","")], "")
+        svgVals = Polyline([0.0, 3.0], styleprops, None)
     }
 
-let toFile (modName, svgString) = writeStringToFile (sprintf "D:/Users/naim/Real Document/Project/University/Year_3/HLP/Group_Project/hlp2020-verilog/ng2517/outputsvg/%s.svg" modName) svgString
+let toFile (modName, svgString) = writeStringToFile (sprintf "../../../outputsvg/%s.svg" modName) svgString
 
 [<EntryPoint>]
 let main argv =    
     let out = Waveform.ModuleWaveform (testInput,state)
-    let outString = output out.svgVals style false
+    let outString = output out.svgVals "" false
     ("Test",outString) |> toFile
 
     0 // return an integer exit code
--- ng2517/wave_types.fs
+++ ng2517/wave_types.fs
@@ -4,5 +4,25 @@ open Verishot.SVG
 
 type WaveformState = 
         //Used to keep track of a wave date and svg coordinates when producing waveform
-        { prevVal: int; 
+        { prevVal: int 
           svgVals: SVGElement}
+
+type WireWaveform =
+    { portName: SVGElement
+      wave: SVGElement}
+
+type BusWaveform =
+    { portName: SVGElement
+      waveList: (SVGElement*SVGElement) list}
+
+type PortWaveform = BusWave of BusWaveform | WireWave of WireWaveform
+
+type SimulatorWire =
+    { portName: string
+      output: int list}
+
+type SimulatorBus =
+    { portName: string
+      outputList: (int*(int list)) list}
+
+type SimulatorPort = SimBus of SimulatorBus | SimWire of SimulatorWire
