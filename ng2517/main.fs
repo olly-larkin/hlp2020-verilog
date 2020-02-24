063a21d43ed652d3ee27c11e1e6f3243cc9dcb1d
NGovani <naim@govani.org>
2020-02-24 02:14:27 +0000

    int list as input  outputs waveform

--- ng2517/main.fs
+++ ng2517/main.fs
@@ -4,22 +4,32 @@ module Verishot.Main
 
 
 open System
+open System.IO
 open Verishot.Waveform
 open Verishot.CoreTypes
+open Verishot.SVG
+open Verishot.Util
+open WaveTypes
 
 
 let testInput = [
-
+    0;1;0;1;1;1;0;0;1
 ]
+
+let style = "fill:none;stroke:black;stroke-width:1"
 
 let state:WaveformState =
     {
         prevVal=0;
-        coords=(10,01);
-        svgVals = []
+        svgVals = Polyline([0.0, 3.0],[("","")], "")
     }
+
+let toFile (modName, svgString) = writeStringToFile (sprintf "D:/Users/naim/Real Document/Project/University/Year_3/HLP/Group_Project/hlp2020-verilog/ng2517/outputsvg/%s.svg" modName) svgString
 
 [<EntryPoint>]
 let main argv =    
-    List.map Waveform.ModuleWaveform testInput
+    let out = Waveform.ModuleWaveform (testInput,state)
+    let outString = output out.svgVals style false
+    ("Test",outString) |> toFile
+
     0 // return an integer exit code
--- ng2517/waveform.fs
+++ ng2517/waveform.fs
@@ -1,57 +1,86 @@
 module Verishot.Waveform
 
 open Verishot.CoreTypes
+open Verishot.SVG
+open WaveTypes
 
 
 
 module Waveform =
+       
+    let addXY (inp:Coord) (x:float) (y:float) = 
+        Coord(fst(inp) + x, snd(inp) + y)
 
     let Low2Low state:WaveformState = 
         //print some SVG using state
         //return something with type state but with 0 as the prevVal
-        let calcLine (w:WaveformState) : string list  = //should return list of SVG lines
-            ["hello"]
-        let tmp = calcLine state
-        let change2Low (w:WaveformState) = {w with prevVal = 0}
-        let addLow2Low  (lines:string list) (w:WaveformState) = {w with svgVals = w.svgVals @ lines}
+        let createLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let newPolyline = match w.svgVals with
+                                | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 3.0 0.0],y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newPolyline}
 
-        state |> change2Low |> addLow2Low tmp
+        let change2Low (w:WaveformState) = {w with prevVal = 0}
+        let addLow2Low (w:WaveformState) = createLine w
+
+        state |> change2Low |> addLow2Low
 
     let Low2High state = 
-        let calcLine (w:WaveformState) : string list  = //should return list of SVG lines
-            ["hello"]
-        let tmp = calcLine state
-        let change2High (w:WaveformState) = {w with prevVal = 1}
-        let addLow2High  (lines:string list) (w:WaveformState) = {w with svgVals = w.svgVals @ lines}
+        //print some SVG using state
+        //return something with type state but with 0 as the prevVal
+        let createLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let newPolyline = match w.svgVals with
+                                | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 0.0 -2.4; addXY endPoint 3.0 -2.4 ],y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newPolyline}
 
-        state |> change2High |> addLow2High tmp
+        let change2High (w:WaveformState) = {w with prevVal = 1}
+        let addLow2High(w:WaveformState) = createLine w
+
+        state |> change2High |> addLow2High
+
     
     let High2Low state = 
-        let calcLine (w:WaveformState) : string list  = //should return list of SVG lines
-            ["hello"]
-        let tmp = calcLine state
-        let change2Low (w:WaveformState) = {w with prevVal = 0}
-        let addHigh2Low  (lines:string list) (w:WaveformState) = {w with svgVals = w.svgVals @ lines}
+        //print some SVG using state
+        //return something with type state but with 0 as the prevVal
+        let createLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let newPolyline = match w.svgVals with
+                                | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 0.0 0.0; addXY endPoint 3.0 0.0;],y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newPolyline}
 
-        state |> change2Low |> addHigh2Low tmp
+        let change2Low (w:WaveformState) = {w with prevVal = 0}
+        let addHigh2Low (w:WaveformState) = createLine w
+
+        state |> change2Low |> addHigh2Low
+
 
     let High2High state = 
-        let calcLine (w:WaveformState) : string list  = //should return list of SVG lines
-            ["hello"]
-        let tmp = calcLine state
+        let createLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let newPolyline = match w.svgVals with
+                                | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 3.0 -2.4],y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newPolyline}
+
         let change2High (w:WaveformState) = {w with prevVal = 1}
-        let addHigh2High  (lines:string list) (w:WaveformState) = {w with svgVals = w.svgVals @ lines}
+        let addHigh2High (w:WaveformState) = createLine w
 
-        state |> change2High |> addHigh2High tmp
+        state |> change2High |> addHigh2High
 
 
-    let SingleCycle state cycle = 
-        match (state.prevVal, cycle.val) with
+
+    let SingleCycle state (cycle:int) = 
+        match (state.prevVal, cycle) with
         | 0,0 -> Low2Low state
         | 0,1 -> Low2High state
         | 1,0 -> High2Low state
         | 1,1 -> High2High state
+        | _ -> state // DEFO NEEDS TO BE CHANGED ADD ERROS
 
-    let ModuleWaveform (moduleSim, initialState:WaveformState) = 
+    let ModuleWaveform (moduleSim:int list, initialState:WaveformState) = 
         //returns a list of 
         (initialState, moduleSim) ||> List.fold SingleCycle
