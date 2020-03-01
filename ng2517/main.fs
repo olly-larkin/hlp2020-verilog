0e30c3dbdfd4842ef03908e7668fae2d3a93a0a8
NGovani <naim@govani.org>
2020-03-01 21:59:22 +0000

    busses now correctly show output

--- ng2517/main.fs
+++ ng2517/main.fs
@@ -16,7 +16,8 @@ let testInput = [
     SimWire{portName = "a"; output = [1;0;0;1;0;1;1;0]};
     SimWire{portName = "b"; output = [0;1;0;1;0;1;0;1]};
     SimWire{portName = "out"; output = [1;1;1;1;0;0;0;0]};
-    SimBus{portName = "reg1"; range = 4; output = [0;1;2;3;4;5;6;7] };
+    SimBus{portName = "reg1"; range = 5; output = [0;0;2;2;4;4;13;20] };
+    SimBus{portName = "reg2"; range = 5; output = [0;1;2;2;10;15;31;31] };
 ]
 
 
--- ng2517/waveform.fs
+++ ng2517/waveform.fs
@@ -14,19 +14,20 @@ module Waveform =
 
     let textBox name = 
         Group([Rectangle((0.0,0.0),(9.0, 4.0) ,[("fill", "none"); ("stroke","black")], Some name);
-        Text((1.0,2.2),name,[("fill", "black")],Some name)],[],None) //standard style for waveform labels
+            Text((1.0,2.2),name,[("fill", "black")],Some name)],[],None) //standard style for waveform labels
                              
     let wrappedWave waveform = 
         let waveDimension = getDimensionElem waveform
         Group([Rectangle((0.0,0.0),(fst(snd(waveDimension))+0.5,4.0),[("fill", "none"); ("stroke","black")], None);
                translateSVG (0.5,0.5) waveform], [], None)               
 
-    let initState:WaveformState = {prevVal=0; svgVals = Polyline([0.0, 3.0], styleprops, None)}
+    let initWireState:WaveformState = {prevVal=0; svgVals = Polyline([0.0, 3.0], styleprops, None)}
+    let initBusState:WaveformState = {prevVal=0; svgVals = Group([Polyline([0.0, 3.0], styleprops, None)], styleprops, None)}
 
     let Low2Low state:WaveformState = 
         //print some SVG using state
         //return something with type state but with 0 as the prevVal
-        let createLine (w:WaveformState)  = 
+        let addLine (w:WaveformState)  = 
             let endPoint = snd(getDimensionElem w.svgVals)
             let newPolyline = match w.svgVals with
                                 | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 5.0 0.0],y ,z)
@@ -34,14 +35,14 @@ module Waveform =
             {w with svgVals = newPolyline}
 
         let change2Low (w:WaveformState) = {w with prevVal = 0}
-        let addLow2Low (w:WaveformState) = createLine w
-
-        state |> change2Low |> addLow2Low
+        state 
+        |> change2Low 
+        |> addLine
 
     let Low2High state = 
         //print some SVG using state
         //return something with type state but with 0 as the prevVal
-        let createLine (w:WaveformState)  = 
+        let addLine (w:WaveformState)  = 
             let endPoint = snd(getDimensionElem w.svgVals)
             let newPolyline = match w.svgVals with
                                 | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 0.0 -3.0; addXY endPoint 5.0 -3.0 ],y ,z)
@@ -49,14 +50,15 @@ module Waveform =
             {w with svgVals = newPolyline}
 
         let change2High (w:WaveformState) = {w with prevVal = 1}
-        let addLow2High(w:WaveformState) = createLine w
 
-        state |> change2High |> addLow2High
+        state 
+        |> change2High 
+        |> addLine
 
     let High2Low state = 
         //print some SVG using state
         //return something with type state but with 0 as the prevVal
-        let createLine (w:WaveformState)  = 
+        let addLine (w:WaveformState)  = 
             let endPoint = snd(getDimensionElem w.svgVals)
             let newPolyline = match w.svgVals with
                                 | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 0.0 0.0; addXY endPoint 5.0 0.0;],y ,z)
@@ -64,12 +66,13 @@ module Waveform =
             {w with svgVals = newPolyline}
 
         let change2Low (w:WaveformState) = {w with prevVal = 0}
-        let addHigh2Low (w:WaveformState) = createLine w
 
-        state |> change2Low |> addHigh2Low
+        state 
+        |> change2Low 
+        |> addLine
 
     let High2High state = 
-        let createLine (w:WaveformState)  = 
+        let addLine (w:WaveformState)  = 
             let endPoint = snd(getDimensionElem w.svgVals)
             let newPolyline = match w.svgVals with
                                 | Polyline(x, y, z) -> Polyline(x @ [addXY endPoint 5.0 -3.0],y ,z)
@@ -77,30 +80,74 @@ module Waveform =
             {w with svgVals = newPolyline}
 
         let change2High (w:WaveformState) = {w with prevVal = 1}
-        let addHigh2High (w:WaveformState) = createLine w
 
-        state |> change2High |> addHigh2High
+        state 
+        |> change2High
+        |> addLine
+    
+    let busSameVal state newVal =
+        let addLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let busLine = [Polyline([addXY endPoint 0.0 -3.0; addXY endPoint 5.0 -3.0],[],None);Polyline([endPoint; addXY endPoint 5.0 0.0],[],None)]
+            let newBusWave = match w.svgVals with
+                                | Group(x, y, z) -> Group(x @ busLine ,y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newBusWave}
+        
+        let updateState (w:WaveformState) = {w with prevVal = newVal}
 
-    let SingleCycle state (cycle:int) = 
+        state 
+        |> updateState
+        |> addLine
+    
+    let busChangeVal state newVal =
+        let addLine (w:WaveformState)  = 
+            let endPoint = snd(getDimensionElem w.svgVals)
+            let busLine = [Polyline([addXY endPoint 0.0 -3.0; addXY endPoint 0.5 0.0; addXY endPoint 5.0 0.0],[],None); Polyline([endPoint; addXY endPoint 0.5 -3.0; addXY endPoint 5.0 -3.0],[],None)]
+            let busVal = "0x" + (sprintf "%X" newVal)
+            let busLineAndVal = busLine @ [Text((addXY endPoint 0.6 -1.1),busVal,[("fill", "black")],None)]
+            let newBusWave = match w.svgVals with
+                                | Group(x, y, z) -> Group(x @ busLineAndVal ,y ,z)
+                                | _ -> w.svgVals // DEFO NEEDS TO BE CHANGED ADD ERROS
+            {w with svgVals = newBusWave}
+        
+        let updateState (w:WaveformState) = {w with prevVal = newVal}
+
+        state 
+        |> updateState
+        |> addLine
+    
+
+    let SingleWireCycle state (cycle:int) = 
         match (state.prevVal, cycle) with
         | 0,0 -> Low2Low state
         | 0,1 -> Low2High state
         | 1,0 -> High2Low state
         | 1,1 -> High2High state
         | _ -> state // DEFO NEEDS TO BE CHANGED ADD ERROS
+        
+    let SingleBusCycle state (newVal:int) = 
+        match state.prevVal = newVal with
+        | true  -> busSameVal state newVal
+        | false -> busChangeVal state newVal
 
-    let GenPortWaveform (initialState:WaveformState) (portVals:int list) = 
-        (initialState, portVals) ||> List.fold SingleCycle
+
+    let GenPortWireWaveform (initialState:WaveformState) (portVals:int list) = 
+        (initialState, portVals) ||> List.fold SingleWireCycle
+    
+    let GenPortBusWaveform (initialState:WaveformState) (portVals:int list) = 
+        (initialState, portVals) ||> List.fold SingleBusCycle
+
 
     let GenWireWaveform (portName:string) (vals:int list) =
-        let waveform = GenPortWaveform initState vals
+        let waveform = GenPortWireWaveform initWireState vals
         let wireBlock = Group([textBox portName; translateSVG (9.0,0.0) (wrappedWave waveform.svgVals)],[], None)
         {waveBlock = wireBlock}
 
     let GenBusWaveform (portName:string) (portRange: int) (vals:int list) =
         let pinName pinNo = portName + "[" + string pinNo + "]"
         let decToBinary (dec:int) (idx:int)  = (idx, dec%2), dec/2 
-        let singlePortWaveform = GenPortWaveform initState
+        let singlePortWaveform = GenPortWireWaveform initWireState
         let keyCheck (map: Map<int, int list>) (idx:int) = if map.ContainsKey idx then map.[idx] else []
         let addPinValToMap (map: Map<int, int list>) (pinVal:(int*int)) = map.Add ((fst(pinVal)),((keyCheck map (fst(pinVal))) @ [snd(pinVal)]))
         let addCycleToMap (busWaveforms: Map<int, int list>) (decVal: (int*int) list) = (busWaveforms, decVal) ||> List.fold addPinValToMap 
@@ -121,10 +168,17 @@ module Waveform =
                 |> (@) [textBox (pinName (fst(inp)))]
             Group(elemList,[], None)
         let individualPortWaveform (offset:float) (inp:(int * int list)) = translateSVG (0.0,4.0 * offset) (createWaveBox inp) , (offset + 1.0)
+        let busBox  = 
+            let elemList = 
+                [(GenPortBusWaveform initBusState vals).svgVals
+                    |> wrappedWave
+                    |> translateSVG (9.0,0.0)]
+                |> (@) [textBox portName]
+            Group(elemList,[], None)
         let blockList =
             List.mapFold individualPortWaveform 1.0 decToWaveformList
             |> fst
-            |> (@) [textBox portName]
+            |> (@) [busBox]
         {waveBlock = Group(blockList,[], None)}
 
     let SetPortPosition (offset:float) (port:PortWaveform) = 
@@ -140,9 +194,9 @@ module Waveform =
     let GenClock (waveformList:SVGElement) =
         let viewerDimensions = getDimensionElem(waveformList)
         let noOfCylces = int((fst(snd(viewerDimensions)) - fst(fst(viewerDimensions)) - 9.5) / 5.0) // (MaxX - MinX - offsets)/width per cycle to get the number of cycles
-        let clkWaveform = List.fold clkCycle (Polyline([0.0, 3.0], styleprops, None)) [1..noOfCylces]
+        let clkWaveform = 
+            ((Polyline([0.0, 3.0], styleprops, None)), [1..noOfCylces]) ||> List.fold clkCycle 
         translateSVG (0.0,snd(snd(viewerDimensions))) (Group([textBox "clk"; translateSVG (9.0,0.0) (wrappedWave clkWaveform)],[], None))
-
 
     let SimOutputToWaveform (inp:SimulatorPort list) =
         let portToWaveform (prt:SimulatorPort) =
