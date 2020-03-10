8de3b77c307759b2fb99c6f64a234d670916a860
Lin Hao Lee <lhlee888@hotmail.com>
2020-03-10 12:23:06 +0000

    complete extension

---
+++ team/Verishot/Program.fs
@@ -0,0 +1,46 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let exitCodes: Map<string, int> = 
+    Map [
+        ("Success", 0)
+        ("InvalidCmd", 1)
+    ]
+
+let help =
+    printfn "verishot <filePath> <flag> <outputPath>"
+
+let throwInvalidCmd = 
+    printfn "Invalid command: run `verishot --help` for a guide."
+
+
+[<EntryPoint>]
+let main argv =
+
+    (*
+        verishot <filePath> <flag> <outputPath>
+        <flag>: --simulate or --visualise
+    *)
+
+    match Array.length argv with
+    | 1 when argv.[0] = "--help" -> 
+        help
+        exitCodes.["Success"]
+    | 3 -> 
+        let filePath = argv.[0]
+        let flag = argv.[1]
+        let workspacePath = argv.[2]
+
+        match flag with 
+        | "--simulate" -> 
+            failwith "TODO"
+        | "--visualise" ->
+            failwith "TODO"
+        | _ -> 
+            throwInvalidCmd
+
+        exitCodes.["Success"]
+    | _ -> 
+        throwInvalidCmd
+        exitCodes.["InvalidCmd"]
--- team/vscode-extension/src/extension.ts
+++ team/vscode-extension/src/extension.ts
@@ -1,7 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
+
+enum VerishotMode {
+	simulate,
+	visualise,
+}
+
+const checkFilePath = (filePath: string | undefined): boolean => {
+	if (!filePath) {
+		vscode.window.showErrorMessage("ERROR: No active file found. Please open a `.v` Verilog file");
+		return false;
+	}
+	else if (filePath.substr(filePath.length - 2) !== ".v") {
+		vscode.window.showErrorMessage("ERROR: Only `.v` Verilog files are allowed");
+		return false;
+	}
+	return true;
+};
+
+const execVerishot = (verishotMode: number) => {
+	const filePath = vscode.window.activeTextEditor?.document.fileName;
+	const workspacePath = vscode.workspace.rootPath;
+
+	if (!checkFilePath(filePath)) {
+		return;
+	}
+	if (verishotMode === VerishotMode.simulate) {
+		const terminalName = "Verishot: Simulate";
+		const terminal = vscode.window.createTerminal(terminalName);
+		terminal.show();
+		terminal.sendText(`verishot ${filePath} --simulate ${workspacePath}`);
+	}
+	else if (verishotMode === VerishotMode.visualise) {
+		const terminalName = "Verishot: Visualise";
+		const terminal = vscode.window.createTerminal(terminalName);
+		terminal.show();
+		terminal.sendText(`verishot ${filePath} --visualise ${workspacePath}`);
+	}
+	else {
+		vscode.window.showErrorMessage("ERROR: Something went wrong...");
+	}
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,27 +53,13 @@ export const activate = (context: vscode.ExtensionContext) => {
 
 	const simulate = vscode.commands.registerCommand('extension.simulate', () => {
 		vscode.window.showInformationMessage('Simulating...');
-		
-		const terminalName = "Verishot: Simulate";
-		const file = vscode.window.activeTextEditor?.document.fileName;
-
-		const terminal = vscode.window.createTerminal(terminalName);
-		terminal.show();
-		terminal.sendText("HelloWorld file");
-		
+		execVerishot(VerishotMode.simulate);
 	});
 	context.subscriptions.push(simulate);
 
 	const visualise = vscode.commands.registerCommand('extension.visualise', () => {
 		vscode.window.showInformationMessage('Visualising...');
-
-		const terminalName = "Verishot: Visualise";
-		const file = vscode.window.activeTextEditor?.document.fileName;
-
-		const terminal = vscode.window.createTerminal(terminalName);
-
-		terminal.show();
-		terminal.sendText("HelloWorld file");
+		execVerishot(VerishotMode.visualise);
 	});
 	context.subscriptions.push(visualise);
 };
