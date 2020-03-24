05c58cc0ebcaa14599da526b9660a81031603837
Lin Hao Lee <lhlee888@hotmail.com>
2020-03-24 20:23:57 +0000

    reformat the --help text

--- team/Verishot/Program.fs
+++ team/Verishot/Program.fs
@@ -21,6 +21,7 @@ GENERAL
 `verishot --help`
     Brings up this usage guide
 
+
 -------
 PROJECT
 -------
@@ -36,6 +37,7 @@ PROJECT
 `verishot --list-modules [.vproj FILE-NAME]
     Lists all modules in the project
 
+    
 -------
 LINTING
 -------
@@ -53,6 +55,8 @@ SIMULATION AND VISUALISATION
 
 `verishot --visualise [.vproj FILE-NAME]`
     Visualises all modules by creating `.svg` files.
+
+    
 "
 
 let invalidCmd = "Invalid command. Run `verishot --help` for a usage guide. "
