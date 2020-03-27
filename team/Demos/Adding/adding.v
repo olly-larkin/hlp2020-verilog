ffcb22c85161e768b179a3fb816db85fd1978af3
Olly Larkin <ollylemonlarkin@gmail.com>
2020-03-27 09:36:34 +0000

    changed comments (but still doesn't seem to work?)

--- team/Demos/Adding/adding.v
+++ team/Demos/Adding/adding.v
@@ -4,10 +4,10 @@ module adding (
     out
 );
 
-input[7:0] in1;
-input[7:0] in2;
-output[7:0] out;
+// input[7:0] in1;
+// input[7:0] in2;
+// output[7:0] out;
 
-assign out = in1 + in2;
+// assign out = in1 + in2;
 
 endmodule
